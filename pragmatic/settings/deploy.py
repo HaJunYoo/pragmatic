104f6f943685fb1e2e723375b1c356604fd44b0d
HaJunYoo <hajuny129@gmail.com>
2022-11-18 15:45:54 +0900

    ✨ feat: deploy.py add docker secret

--- pragmatic/settings/deploy.py
+++ pragmatic/settings/deploy.py
@@ -1,11 +1,11 @@
 from .base import *
 
-# def read_secret(secret_name):
-#     file = open('/run/secrets/' + secret_name)
-#     secret = file.read()
-#     secret = secret.rstrip().lstrip()
-#     file.close()
-#     return secret
+def read_secret(secret_name):
+    file = open('/run/secrets/' + secret_name)
+    secret = file.read()
+    secret = secret.rstrip().lstrip()
+    file.close()
+    return secret
 
 env = environ.Env(
     # set casting, default value
@@ -21,10 +21,10 @@ environ.Env.read_env(
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+# SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ['*']
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -44,8 +45,9 @@ DATABASES = {
         'PORT': '3306',
     }
 }
-
 '''
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -56,4 +58,3 @@ DATABASES = {
         'PORT': '3306',
     }
 }
-'''
